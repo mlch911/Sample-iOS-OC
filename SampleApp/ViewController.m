f028e4030a7a195dc2ef00e9c522f2db12cca6c7
mlch911 <mlch911@mluoc.tech>
2019-05-01 15:21:01 +0800

    Ver 0.0.3
    UITableViewDataSource

--- SampleApp/ViewController.m
+++ SampleApp/ViewController.m
@@ -8,36 +8,36 @@
 
 #import "ViewController.h"
 
-@interface TestView : UIView
+//@interface TestView : UIView
+//
+//@end
+//
+//@implementation TestView
+//
+//- (instancetype)init {
+//    self = [super init];
+//    if (self) {
+//
+//    }
+//    return self;
+//}
+//
+//- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
+//    [super willMoveToSuperview: newSuperview];
+//}
+//- (void)didMoveToSuperview{
+//    [super didMoveToSuperview];
+//}
+//- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
+//    [super willMoveToWindow: newWindow];
+//}
+//- (void)didMoveToWindow{
+//    [super didMoveToWindow];
+//}
+//
+//@end
 
-@end
-
-@implementation TestView
-
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        
-    }
-    return self;
-}
-
-- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
-    [super willMoveToSuperview: newSuperview];
-}
-- (void)didMoveToSuperview{
-    [super didMoveToSuperview];
-}
-- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
-    [super willMoveToWindow: newWindow];
-}
-- (void)didMoveToWindow{
-    [super didMoveToWindow];
-}
-
-@end
-
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource>
 
 @end
 
@@ -71,6 +71,30 @@
     
     UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
     [self.view addSubview:tableView];
+    
+    tableView.rowHeight = 200;
+    tableView.dataSource = self;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    创建可重用cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"id"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"id"];
+    }
+    
+    cell.textLabel.text = [[NSString alloc] initWithFormat:@"主标题：第%ld行", (long)indexPath.row + 1];
+    cell.detailTextLabel.text = [[NSString alloc] initWithFormat:@"副标题：第%ld行", (long)indexPath.row + 1];
+    cell.imageView.image = [UIImage imageNamed:@"icon.bundle/video@2x.png"];
+    
+    return cell;
 }
 
 @end
